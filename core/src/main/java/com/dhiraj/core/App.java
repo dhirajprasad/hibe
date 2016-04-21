7004bf3aebcd2391a80fb3a6fc4ccb4af767ffb3
dhirajprasad <maildhirajprasad@gmail.com>
2016-04-21 23:47:31 +0530

    initial commit

---
+++ core/src/main/java/com/dhiraj/core/App.java
@@ -0,0 +1,13 @@
+package com.dhiraj.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
